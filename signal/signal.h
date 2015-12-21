3f836b773aad260ae13c174e362fa770731e3122
Hanks.Wang <byahankswang@gmail.com>
2015-12-21 17:34:24 +0800

    Delete none-valuable information.

--- signal/signal.h
+++ signal/signal.h
@@ -3,14 +3,6 @@
  *
  */
 
-#ifndef _UAPI__ASM_GENERIC_SIGNAL_H
-#define _UAPI__ASM_GENERIC_SIGNAL_H
-
-#include <linux/types.h>
-
-#define _NSIG		64
-#define _NSIG_BPW	__BITS_PER_LONG
-#define _NSIG_WORDS	(_NSIG / _NSIG_BPW)
 
 #define SIGHUP		 1
 #define SIGINT		 2
@@ -55,72 +47,3 @@
 #ifndef SIGRTMAX
 #define SIGRTMAX	_NSIG
 #endif
-
-/*
- * SA_FLAGS values:
- *
- * SA_ONSTACK indicates that a registered stack_t will be used.
- * SA_RESTART flag to get restarting signals (which were the default long ago)
- * SA_NOCLDSTOP flag to turn off SIGCHLD when children stop.
- * SA_RESETHAND clears the handler when the signal is delivered.
- * SA_NOCLDWAIT flag on SIGCHLD to inhibit zombies.
- * SA_NODEFER prevents the current signal from being masked in the handler.
- *
- * SA_ONESHOT and SA_NOMASK are the historical Linux names for the Single
- * Unix names RESETHAND and NODEFER respectively.
- */
-#define SA_NOCLDSTOP	0x00000001
-#define SA_NOCLDWAIT	0x00000002
-#define SA_SIGINFO	0x00000004
-#define SA_ONSTACK	0x08000000
-#define SA_RESTART	0x10000000
-#define SA_NODEFER	0x40000000
-#define SA_RESETHAND	0x80000000
-
-#define SA_NOMASK	SA_NODEFER
-#define SA_ONESHOT	SA_RESETHAND
-
-/*
- * New architectures should not define the obsolete
- *	SA_RESTORER	0x04000000
- */
-
-#if !defined MINSIGSTKSZ || !defined SIGSTKSZ
-#define MINSIGSTKSZ	2048
-#define SIGSTKSZ	8192
-#endif
-
-#ifndef __ASSEMBLY__
-typedef struct {
-	unsigned long sig[_NSIG_WORDS];
-} sigset_t;
-
-/* not actually used, but required for linux/syscalls.h */
-typedef unsigned long old_sigset_t;
-
-#include <asm-generic/signal-defs.h>
-
-#ifdef SA_RESTORER
-#define __ARCH_HAS_SA_RESTORER
-#endif
-
-#ifndef __KERNEL__
-struct sigaction {
-	__sighandler_t sa_handler;
-	unsigned long sa_flags;
-#ifdef SA_RESTORER
-	__sigrestore_t sa_restorer;
-#endif
-	sigset_t sa_mask;		/* mask last for extensibility */
-};
-#endif
-
-typedef struct sigaltstack {
-	void __user *ss_sp;
-	int ss_flags;
-	size_t ss_size;
-} stack_t;
-
-#endif /* __ASSEMBLY__ */
-
-#endif /* _UAPI__ASM_GENERIC_SIGNAL_H */
